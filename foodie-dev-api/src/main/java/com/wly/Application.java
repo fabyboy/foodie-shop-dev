83f4e20abf95dbc1ce5dacb029418608970d7ec3
lovefaby@163.com <lovefaby@163.com>
2020-03-22 16:18:48 +0800

    子工程api helloworld!

---
+++ foodie-dev-api/src/main/java/com/wly/Application.java
@@ -0,0 +1,16 @@
+package com.wly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangliyong
+ * description com.wly
+ * @data 2020/3/22
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
