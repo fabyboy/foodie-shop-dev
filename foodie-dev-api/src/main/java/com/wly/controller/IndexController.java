750bdf7fef835cd83f1a51814b3ab825a1986171
lovefaby@163.com <lovefaby@163.com>
2020-04-09 23:04:57 +0800

    首页轮播图功能实现

---
+++ foodie-dev-api/src/main/java/com/wly/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.wly.controller;
+
+import com.wly.enums.YesOrNo;
+import com.wly.pojo.Carousel;
+import com.wly.service.CarouselService;
+import com.wly.utils.JSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(value = "首页", tags = {"首页展示的相关接口"})
+@RestController
+@RequestMapping("index")
+public class IndexController {
+
+    @Autowired
+    private CarouselService carouselService;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public JSONResult carousel() {
+        List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+        return JSONResult.ok(list);
+    }
+}
---
+++ foodie-dev-service/src/main/java/com/wly/service/impl/CarouselServiceImpl.java
@@ -0,0 +1,33 @@
+package com.wly.service.impl;
+
+import com.wly.mapper.CarouselMapper;
+import com.wly.pojo.Carousel;
+import com.wly.service.CarouselService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class CarouselServiceImpl implements CarouselService {
+
+    @Autowired
+    private CarouselMapper carouselMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Carousel> queryAll(Integer isShow) {
+
+        Example example = new Example(Carousel.class);
+        example.orderBy("sort").desc();
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("isShow", isShow);
+
+        List<Carousel> result =  carouselMapper.selectByExample(example);
+
+        return result;
+    }
+}
